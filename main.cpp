0ba706426f59659e1e3a4b653c45b4651bf4f78f
linosk <linakoskar@gmail.com>
2023-09-29 20:53:30 +0200

    Find away to make matrix class make sense

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
-#include <array>
+#include <vector>
 
 class Matrix{
 	public:
 		int Rows;
 		int Columns;
-		//std::array<std::array<int,Matrix::Columns>,Matrix::Rows> Matrix;
-		std::array < std::array<int, Matrix::Rows>, 3 > array_name ;
-		
+
+		std::vector<std::vector<int>> MAT;
+
+		Matrix(int Rows, int Columns) : MAT(Rows, std::vector<int>(Columns)){
+
+		}
+
 };
 
 int main(int argc, char** argv){
+
+	Matrix(3,3);
+
 	return 0;
 }
