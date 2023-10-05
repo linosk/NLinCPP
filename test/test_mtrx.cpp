d33a9c313d23691841d3b695244f4cff613be052
linosk <linakoskar@gmail.com>
2023-10-05 19:41:01 +0200

    Fill works

--- test/test_mtrx.cpp
+++ test/test_mtrx.cpp
@@ -4,11 +4,11 @@
 
 TEST(TEST_mtrx,MTRX_Fill){
 
-    //MTRX A(1,1);
-    //A.Matrix[1][1] = 1;
+    MTRX A(3,3);
+    A.Fill(1);
 
-    //MTRX B(1,1);
-    //B.Matrix[1][1] = 1;
+    MTRX B(3,3);
+    B.Fill(1);
 
-    EXPECT_TRUE(true);
+    EXPECT_TRUE(Compare(A,B));
 }
