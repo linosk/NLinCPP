591fbf56dc8eac310b15376eb9492e033a98d3c7
linosk <linakoskar@gmail.com>
2023-10-13 20:40:55 +0200

    More clear view of class MNIST_READER

--- include/mnist_reader.h
+++ include/mnist_reader.h
@@ -6,8 +6,12 @@
 class MNIST_READER{
     public:
 
+        int Number_of_images;
+
         const int Height;
         const int Width;
+
+        std::vector<int> Label;
 
         MTRX Matrix;
         MNIST_READER(int M, int N);
