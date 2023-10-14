ae7df5debabc994f1c71144a31377a4aa368c9d7
linosk <linakoskar@gmail.com>
2023-10-14 18:02:53 +0200

    Some error, for now repair mrtx

--- include/mnist_reader.h
+++ include/mnist_reader.h
@@ -3,18 +3,36 @@
 
 #include "mtrx.h"
 
+#include <string>
+#include <stdexcept>
+
+//typedef struct MNIST_READER_TYPE {};
+enum MNIST_READER_TYPE {};
+
+int Training_size;
+int Test_size;
+int Pixels_for_image;
+
+std::string Training_data_location;
+std::string Training_label_location;
+std::string Test_data_location;
+std::string Test_label_location;
+
 class MNIST_READER{
     public:
 
         int Number_of_images;
+        MNIST_READER_TYPE Data_set_type;
 
         const int Height;
         const int Width;
 
-        std::vector<int> Label;
+        Vector Label;
 
         MTRX Matrix;
-        MNIST_READER(int M, int N);
+        MNIST_READER(MNIST_READER_TYPE Type);
+
+        void Set_type(MNIST_READER_TYPE Type);
 };
 
 #endif
--- src/mnist_reader.cpp
+++ src/mnist_reader.cpp
@@ -1,10 +1,58 @@
 #include "mnist_reader.h"
 
-MNIST_READER::MNIST_READER(int M, int N) : 
-Matrix(M,N), 
+/*
+enum MNIST_READER_TYPE {
+    Training = 0,
+    Test
+};
+
+const int Training_size = 60000;
+const int Test_size = 10000;
+const int Pixels_for_image = 768;
+
+//Program exepcts input files to be unzipped and in specific location
+
+const std::string Training_data_location = "../data/train-images.idx3-ubyte";
+const std::string Training_label_location = "../data/train-labels.idx1-ubyte";
+const std::string Test_data_location = "../data/t10k-images.idx3-ubyte";
+const std::string Test_label_location = "../data/t10k-labels.idx1-ubyte";
+
+MNIST_READER::MNIST_READER(MNIST_READER_TYPE Type) :  
+Label(1),
+Matrix(1,1),
 Height(28), 
-Width(28){
+Width(28),
+Data_set_type(Type){
 
-
+    Set_type(Type);
 
 }
+
+void MNIST_READER::Set_type(MNIST_READER_TYPE Type){
+    
+    switch(Type){
+        case 0:{
+
+            Number_of_images = Training_size;
+            Label.resize(Training_size,0);
+
+            break;
+        }
+        case 1:{
+
+            Data_set_type = Type;
+            Number_of_images = Training_size;
+            Label.resize(Training_size,0);
+
+            break;
+        }
+        default:{
+            		
+            throw std::invalid_argument("Choose other dataset type.");
+
+            break;
+        }
+    }
+
+}
+*/
