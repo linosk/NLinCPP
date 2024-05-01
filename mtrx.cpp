f9e2ce9b1644f65337a4889693703a0ccfe13570
linosk <oskarlinak@gmail.com>
2024-05-01 18:39:04 +0200

    Defining matrix library

--- mtrx.cpp
+++ mtrx.cpp
@@ -1,6 +1,14 @@
 #include "mtrx.hpp"
+//ALGORITHM FOR MATRIX PRODUCT
+//Define matrix m x n
+MTRX::MTRX(int m, int n) : rows(m), columns(n){
+    matrix.resize(rows,Vector(columns));
+}
+//this should log
+void MTRX::printMatrix(void){
+    for(auto i : matrix){
+        for(auto j : i){
 
-struct matrix{
-    int nRows;
-    int nColumns;
-};
+        }
+    }
+} 
--- mtrx.hpp
+++ mtrx.hpp
@@ -1,6 +1,22 @@
 #ifndef _MTRX_HPP_ // maybe better name?
 #define _MTRX_HPP_
 
-struct matrix{};
+#include <vector>
+
+typedef std::vector<int> Vector;
+typedef std::vector<Vector> Matrix;
+
+class MTRX{
+
+public:
+    int rows;
+    int columns;
+
+    Matrix matrix{};
+    
+    MTRX(int m, int n);
+
+    void printMatrix(void);
+};
 
 #endif//_MTRX_HPP_
