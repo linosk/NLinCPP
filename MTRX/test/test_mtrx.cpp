fa1755e8ccf9474aa44d450365dfdb583c7b0fd1
linosk <linakoskar@gmail.com>
2024-05-09 22:33:00 +0200

    Test configuration works

---
+++ MTRX/test/test_mtrx.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include "mtrx.hpp"
+
+TEST(TEST_MTRX,canBeAdded_True){
+   
+    MTRX A(2,2);
+
+    MTRX B(2,2);
+
+    EXPECT_TRUE(A.canBeAdded(A.dimensions,B.dimensions));
+
+}
+
+TEST(TEST_MTRX,canBeAdded_False){
+    
+    MTRX A(1,1);
+
+    MTRX B(3,3);
+
+    EXPECT_FALSE(A.canBeAdded(A.dimensions,B.dimensions));
+}
