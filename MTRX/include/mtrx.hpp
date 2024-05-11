97a68498e50cac760c588e8e8742be7ec0034f23
linosk <linakoskar@gmail.com>
2024-05-11 18:12:01 +0200

    COmments deleted

--- MTRX/include/mtrx.hpp
+++ MTRX/include/mtrx.hpp
@@ -41,42 +41,6 @@ public:
 
     bool addMatrices(const MTRX A, const MTRX B, const std::string id = "");
     bool dotProduct(const MTRX A, const MTRX B, const std::string id = "");
-
-    // /MTRX(&addMatrices);
-
-    //MTRX(MTRX A,MTRX B,std::function<>);
-
-/*
-private:
-
-    typedef std::vector<int> Vector;
-    typedef std::vector<Vector> Matrix;
-    struct Dimensions{
-        int rows;
-        int columns;
-    };
-
-    int rows;
-    int columns;
-    Dimensions dimensions; 
-    Matrix matrix{};
-
-    Logger logger;
-
-    bool canBeAdded(Dimensions dimensionsFirst, Dimensions dimensionsSecond);
-    bool canBeDotted(Dimensions dimensionsFirst, Dimensions dimensionsSecond);
-    //basically id, necessary for logging purposes
-    std::string name;
-
-public:
-
-    MTRX(int m, int n);
-    MTRX(int m, int n, std::string name);
-
-    void loopMatrix(void);
-    void fillMatrix(int val);
-    Dimensions getDimensions(void);
-    //void printMatrix(void); ---- ?*/
 };
 
 #endif//_MTRX_HPP_
